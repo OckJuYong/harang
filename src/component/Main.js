e45de4d323374d93d98dc7a0c0c0fe80b09f1907
OckJuYong <128788634+OckJuYong@users.noreply.github.com>
2024-10-01 20:00:32 +0900

    Update Main.js

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 
 const Main = () => {
@@ -6,12 +5,3 @@ const Main = () => {
 };
 
 export default Main;
-=======
-import React from "react";
-
-const Main = () => {
-  return <div>Reservation System</div>;
-};
-
-export default Main;
->>>>>>> be85a149f785e778c86f75fad9d90a3dc64d4187
