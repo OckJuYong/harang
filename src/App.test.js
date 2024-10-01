6e04e2e8eb79fd24d6b4fd6c3bb6071eaa99beb6
OckJuYong <128788634+OckJuYong@users.noreply.github.com>
2024-10-01 19:59:40 +0900

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
@@ -8,14 +7,3 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
-=======
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
->>>>>>> be85a149f785e778c86f75fad9d90a3dc64d4187
