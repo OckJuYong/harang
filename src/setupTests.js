a0de705a9a59f04e2acae0fa74748a9501990a06
OckJuYong <128788634+OckJuYong@users.noreply.github.com>
2024-10-01 19:59:28 +0900

    Update setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-=======
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
->>>>>>> be85a149f785e778c86f75fad9d90a3dc64d4187
